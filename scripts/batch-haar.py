07b6755e9a77ffaf86aadb7043d21c3abfd646f7
Nhan Dao <nhan.dao@outlook.com>
2020-07-08 12:47:11 +0800

    now follows flake 8

--- scripts/batch-haar.py
+++ scripts/batch-haar.py
@@ -6,7 +6,7 @@ import argparse
 import cv2
 from tqdm import tqdm
 
-from flt.utils import split_views, mask_boxes, draw_boxes
+from flt.utils import split_views, mask_boxes
 
 
 def filter_largest_rectangle(rects):
@@ -15,10 +15,10 @@ def filter_largest_rectangle(rects):
     Args:
         rects (Tuple): x1, y1, x2, y2
     """
-    calc_area = lambda x: (x[2] - x[0]) * (x[3] - x[1])
-    out = sorted(rects, key=calc_area)
+    # not efficient
+    out = sorted(rects, key=lambda x: (x[2] - x[0]) * (x[3] - x[1]))
     return out[0]
-    
+
 
 def _main(args):
     if not os.path.isdir(args.save):
@@ -27,30 +27,31 @@ def _main(args):
     front_cascade = cv2.CascadeClassifier(
         cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
     )
-    
+
+    views = ['left', 'center', 'right']
+
     for target in tqdm(args.target):
         # read
-        image = cv2.imread(target, 0)
+        image = cv2.imread(target, 0)  # open in gray-scale
 
         # detect & mask
-        for frame, view in zip(split_views(image), ['left', 'center', 'right']):
+        for frame, view in zip(split_views(image), views):
             faces = front_cascade.detectMultiScale(frame)
             face = filter_largest_rectangle(faces)
             res = mask_boxes(frame, [face], invert=True)
 
             # save
-            _, name = os.path.split(target)
-            name, ext = os.path.splitext(name)
-            outname = '{}_{}{}'.format(name, view, ext)
-            cv2.imwrite(os.path.join(args.save, outname), res)
+            name = os.path.basename(target)
+            outfile = '{}_{}{}'.format(name, view, '.png')
+            cv2.imwrite(os.path.join(args.save, outfile), res)
 
 
 def _get_parser():
     parser = argparse.ArgumentParser(
         description="CLI to runs facial detection using Haar-Cascade")
-    parser.add_argument("target", type=str, nargs="+",help="path to image/s")
+    parser.add_argument("target", type=str, nargs="+", help="path to image/s")
     parser.add_argument("-s", "--save", type=str, default="/tmp",nargs='?',
-        help="output directory")
+                        help="output directory")
     return parser
 
 
@@ -62,4 +63,3 @@ if __name__ == '__main__':
         print("Program terminated by user")
 
     cv2.destroyAllWindows()
-        
--- scripts/track-keypoints.py
+++ scripts/track-keypoints.py
@@ -6,24 +6,26 @@ import json
 import cv2
 import numpy as np
 
-MIN_MATCH_COUNT=10
+
+MIN_MATCH_COUNT = 10
+
 
 def create_flann():
     FLANN_INDEX_KDTREE = 0
-    index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
-    search_params = dict(checks = 50)
+    index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+    search_params = dict(checks=50)
 
     return cv2.FlannBasedMatcher(index_params, search_params)
 
 
 def draw_matches(img1, kp1, img2, kp2, good, matchesMask):
     draw_params = dict(
-                matchColor = (0,255,0), # draw matches in green color
-                singlePointColor = None,
-                matchesMask = matchesMask, # draw only inliers
-                flags = 2
+                matchColor=(0, 255, 0),  # draw matches in green color
+                singlePointColor=None,
+                matchesMask=matchesMask,  # draw only inliers
+                flags=2
     )
-    img = cv2.drawMatches(img1,kp1,img2,kp2,good,None,**draw_params)
+    img = cv2.drawMatches(img1, kp1, img2, kp2, good, None, **draw_params)
     return img
 
 
@@ -34,8 +36,8 @@ def _main(args):
         config = json.load(fp)
         detector = cv2.xfeatures2d.SIFT_create(**config['SIFT'])
 
-    # initialise keypoints matcher 
-    #bf = cv2.BFMatcher(normtype=[_args.normtype], crosscheck=False)
+    # initialise keypoints matcher
+    # bf = cv2.BFMatcher(normtype=[_args.normtype], crosscheck=False)
     flann = create_flann()
 
     # detect keypoints of the first frame
@@ -46,53 +48,59 @@ def _main(args):
     # Sliding window of two frames
     for img_path in args.target[1:]:
         img2 = cv2.imread(img_path, 0)
-        
+
         # detect and match
         kp2, des2 = detector.detectAndCompute(img2, None)
-        matches = flann.knnMatch(des1, des2, k=2) # requires k=2 for lowe ratio 
+        matches = flann.knnMatch(des1, des2, k=2)  # k=2 for lowe ratio
 
         # store all the good matches as per Lowe's ratio test.
         good = []
-        for m,n in matches:
+        for m, n in matches:
             if m.distance < args.lowe_ratio * n.distance:
                 good.append(m)
 
         if len(good) > MIN_MATCH_COUNT:
-            src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1,1,2)
-            dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1,1,2)
+            # sample keypoints only in good
+            src_pts = np.float32([kp1[m.queryIdx].pt for m in good])
+            src_pts = src_pts.reshape(-1, 1, 2)
+            dst_pts = np.float32([kp2[m.trainIdx].pt for m in good])
+            dst_pts = dst_pts.reshpae(-1, 2, 2)
 
             M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
             matchesMask = mask.ravel().tolist()
 
-            pts = np.float32([[0,0],[0,h-1],[w-1,h-1],[w-1,0]]).reshape(-1,1,2)
+            pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]])
+            pts = pts.reshape(-1, 1, 2)
             dst = cv2.perspectiveTransform(pts, M)
 
-            img2 = cv2.polylines(img2, [np.int32(dst)],True, 255, 3, cv2.LINE_AA)
+            img2 = cv2.polylines(
+                img2,
+                [np.int32(dst)],
+                True, 255, 3, cv2.LINE_AA
+            )
 
         else:
-            print("Not enough matches are found - %d/%d" %(len(good), MIN_MATCH_COUNT))
+            print("Not enough matches are found - %d/%d" % (
+                len(good), MIN_MATCH_COUNT))
             matchesMask = None
 
         img3 = draw_matches(img1, kp1, img2, kp2, good, matchesMask)
         cv2.imshow('gray', img3)
         cv2.waitKey(0)
         img1 = img2
-        
-    
+
+
 def _get_parser():
     parser = argparse.ArgumentParser(
         description="CLI to track keypoints between a series of images"
     )
-    parser.add_argument("target", type=str, nargs="+",help="path to image/s")
+    parser.add_argument("target", type=str, nargs="+", help="path to image/s")
     parser.add_argument("-cfg", "--config_file", type=str, nargs="?",
-        default="config.json",
-        help="path to configuration file"
-    )
+                        default="config.json",
+                        help="path to configuration file")
     parser.add_argument("--lowe_ratio", type=float, default=0.7, nargs='?',
-        help="lowe's ratio for filtering matches"
-    )
+                        help="lowe's ratio for filtering matches")
     return parser
-    
 
 
 if __name__ == '__main__':
