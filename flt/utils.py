669ef448b003510c7b538670aab52e53cc622662
Nhan Dao <nhan.dao@outlook.com>
2020-07-09 00:05:13 +0800

    flake8 xD

--- flt/utils.py
+++ flt/utils.py
@@ -15,13 +15,13 @@ def split_views(image, l=1/3, r=2/3):
     """
     w = image.shape[1]
     l, r = int(w * l), int(w * r)
-    return image[:,:l,... ],image[:,l:r,... ],image[:,r:,...]
+    return image[:, :l, ...], image[:, l:r, ...], image[:, r:, ...]
 
 
 def draw_boxes(image, boxes):
     res = image.copy()
     for box in boxes:
-        cv2.rectangle(res, box, (0,0,255), 2) 
+        cv2.rectangle(res, box, (0, 0, 255), 2)
     return res
 
 
@@ -34,12 +34,11 @@ def mask_boxes(image, boxes, invert=False):
         invert (bool, optional): Invert the masking. Defaults to True.
     """
 
-    mask = np.zeros(image.shape[:2],dtype=np.uint8)
+    mask = np.zeros(image.shape[:2], dtype=np.uint8)
     for box in boxes:
         cv2.rectangle(mask, box, (255, 255, 255), -1)
 
     if not invert:
         mask = np.invert(mask)
-        
+
     return cv2.bitwise_and(image, image, mask=mask)
-        
